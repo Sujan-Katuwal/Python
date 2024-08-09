38c674140ff046f99b9e73f29ab1f10440634899
Sujan-Katuwal <160639522+Sujan-Katuwal@users.noreply.github.com>
2024-08-08 22:05:12 -0700

    Add files via upload
    Operators inpython

---
+++ operator.py
@@ -0,0 +1,54 @@
+# example of each operator
+
+# Arithmatic operator
+print("Arithmatic Operators.....")
+first = 10
+second = 2
+add = first + second
+sub = first - second
+mul = first * second
+division = first / second
+modulus = first % second
+print("addition is:",add)
+print("subtraction is:",sub)
+print("Multiplication:",mul)
+print("Division is:",division)
+print("Modulus is:",modulus)
+
+# Comaprision Operators
+print("Comaprision Operators.......")
+print(first < second)
+print(first > second)
+print(first == second)
+print(first != second)
+print(first <= second)
+print(first >= second)
+
+# Logical Operators
+print("Logical Operators.............")
+a = True
+b = False
+print(a and b)
+print(a or b)
+print(not a)
+print(not b)
+
+# Bitwise Operators
+print("Bitwise Operators.........")
+#  bitwise AND
+print(first & second)
+# bitwise OR
+print(first | second)
+# bitwise NOT
+print(~first)
+print(~second)
+# bitwise XOR
+print(first ^ second)
+
+# Assignment Operator
+print("Assignment Operators..............")
+x = 5
+y = "Sujan"
+z = x
+print(z)
+print(y)
