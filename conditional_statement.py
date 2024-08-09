d248078212af72dcea77dfa92bb161a87275c11e
Sujan-Katuwal <160639522+Sujan-Katuwal@users.noreply.github.com>
2024-08-08 22:03:53 -0700

    Add files via upload
    Conditional Statement in python

---
+++ conditional_statement.py
@@ -0,0 +1,21 @@
+# age = 18
+# if age>18:
+#  print("adult")
+#  WAP which take into input as age and print they are old or not
+
+age = int(input("Enter age: "))
+name = input("Enter name: ")
+if age>=50:
+ print(" You are Old because u are",age, "years")
+else:
+ print("You are not old because you are",age, "years")
+ print(f"Hello {name} nice to see u")
+
+ value = "90"
+ print("Before Conversion:", type(value))
+ value_conversion = int(value)
+ print("After Conversion: ", type(value_conversion))
+
+
+#  WAP to take user input email and validate whether it is correct or not
+# WAP to take user input mark percentage and find their division
